854e5c077283b425bc19ae9f3be4be0b1872cb92
Srikanth S <srikanth.srs@gmail.com>
2015-09-22 00:29:48 +0530

    Adding test cases for Time.java, and modified the build script to include junit as a dependency for running tests.

---
+++ src/test/java/srikanth/tw/ctm/util/TimeTest.java
@@ -0,0 +1,21 @@
+package srikanth.tw.ctm.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeTest {
+	@Test
+	public void testMinutesToDisplayTime() {
+		assertEquals("01:00 AM", Time.minutesToDisplayTime(1 * 60));
+		assertEquals("12:00 AM", Time.minutesToDisplayTime(0));
+		assertEquals("12:00 PM", Time.minutesToDisplayTime(12 * 60));
+		assertEquals("01:00 PM", Time.minutesToDisplayTime(13 * 60));
+		assertEquals("11:59 PM", Time.minutesToDisplayTime(12 * 60 + 11 * 60 + 59));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testMinutesToDisplayTimeBoundary() {
+		assertEquals("12:00 AM", Time.minutesToDisplayTime(12 * 60 + 12 * 60));
+	}
+}
